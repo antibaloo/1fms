3f50a8a83c551a059ad16b1683cd4d5d88e9c312
root <root@1fms.com>
2017-10-04 04:56:26 +0000

    Удаляем подозрительную строку

--- htdocs/bitrix/modules/fms/codes_init.php
+++ htdocs/bitrix/modules/fms/codes_init.php
@@ -2,6 +2,6 @@
 $_SERVER["DOCUMENT_ROOT"] = realpath(__DIR__.'/../../../');
 require($_SERVER["DOCUMENT_ROOT"].'/bitrix/modules/main/include/prolog_before.php');
 
-$GLOBALS['USER']->Authorize(1);
+//$GLOBALS['USER']->Authorize(1);
 $GLOBALS['s5BitrixCodes']->generate();
 echo "Done\n";
