be17a92dd2dbdeff3d8d3fca5bc857bfc2b994a7
root <root@1fms.com>
2017-10-04 04:54:52 +0000

    Подозрительный файл по мнению Битрис-поиск троянов

---
+++ htdocs/bitrix/modules/fms/codes_init.php
@@ -0,0 +1,7 @@
+<?
+$_SERVER["DOCUMENT_ROOT"] = realpath(__DIR__.'/../../../');
+require($_SERVER["DOCUMENT_ROOT"].'/bitrix/modules/main/include/prolog_before.php');
+
+$GLOBALS['USER']->Authorize(1);
+$GLOBALS['s5BitrixCodes']->generate();
+echo "Done\n";
