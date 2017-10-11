5b8f4956827b7c20541827bf85532c8a9e11c08f
root <root@1fms.com>
2017-10-11 06:42:52 +0000

    Банер на галвное в без авторизации

---
+++ htdocs/bitrix/modules/fms/classes/general/Fms/HtmlParts/TopUnauthImages.php
@@ -0,0 +1,32 @@
+<?
+namespace Fms\HtmlParts;
+
+class TopUnauthImages {
+public static function show () {?>
+	<div class="topUnauthImagesClip">
+		<div class="header_unauth_container">
+			<div class="header_unauth_arrows"></div>
+			<div class="header_unauth_block header_unauth_block1">
+				<div class="header_unauth_block_text">
+					Зарегистрируйся на<br> флэшмоб и сделай<br> пост в соц.сетях
+				</div>
+			</div>
+			<div class="header_unauth_block header_unauth_block2">
+				<div class="header_unauth_block_text">
+					Приди с друзьями в<br> указанное время и<br> получи скидку!
+				</div>
+			</div>
+			<div class="header_unauth_block header_unauth_block3">
+				<div class="header_unauth_block_text">
+					Загрузи фото, сделай<br> пост в соц.сетях и<br> получи приз от<br> молла
+				</div>
+			</div>
+			<div class="header_unauth_main_text_container">
+				<div class="header_unauth_main_text">Флэшмоб распродажи для студентов</div>
+				<div class="header_unauth_fb">
+					<div class="fb-like widget" data-send="false" data-layout="button_count" data-width="450" data-show-faces="true" data-font="lucida grande"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+<?}}
