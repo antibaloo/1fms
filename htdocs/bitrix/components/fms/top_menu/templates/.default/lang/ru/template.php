09b5cc743c32a34b5e1a8666135256f4cec55220
root <root@1fms.com>
2017-10-11 06:29:43 +0000

    Скидки для друзей --> Вместе весело и выгодно

---
+++ htdocs/bitrix/components/fms/top_menu/templates/.default/lang/ru/template.php
@@ -0,0 +1,8 @@
+<?
+//$MESS['LOGO_TEXT'] = 'Скидки для друзей';
+$MESS['LOGO_TEXT'] = 'Вместе весело и выгодно';
+$MESS['SEARCH'] = 'Я ищу...';
+$MESS['SHOPS'] = 'Магазинов';
+$MESS['ADD_NEW_FMS'] = 'Добавить FMS';
+$MESS['PHOTO_REPORTS'] = 'Флэшмоб отчеты';
+//$MESS[''] = '';
