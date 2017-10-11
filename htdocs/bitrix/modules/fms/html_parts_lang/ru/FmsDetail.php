585233eb401f26dacba4b1d9a95ec0b466c97e26
root <root@1fms.com>
2017-10-11 06:13:35 +0000

    Исходный файл констант для перевода

---
+++ htdocs/bitrix/modules/fms/html_parts_lang/ru/FmsDetail.php
@@ -0,0 +1,67 @@
+<?
+$MESS['NEW_FMS'] = 'Новый FMS';
+$MESS['SALE'] = 'Распродажа';
+$MESS['EVENT'] = 'Событие';
+$MESS['FMS_NAME'] = 'Название FMS';
+$MESS['CATEGORY'] = 'Категория';
+
+$MESS['TELL_YOUR_FRIEND'] = 'Расскажи друзьям о флэшмобе';
+$MESS['POINTS_FOR_SHARE'] = 'За каждого пришедшего по твоей ссылке +10 очков';
+
+$MESS['DESCRIPTION'] = 'Описание';
+$MESS['AWARDS'] = 'Награды';
+$MESS['MALL_PRIZES_LIST'] = 'Награды от молла';
+
+$MESS['PART_COND'] = 'Условие участия в флэшмобе';
+$MESS['PART_COND_ticket'] = 'Билет';
+$MESS['PART_COND_action'] = 'Действие';
+$MESS['SPECIAL_MISSION'] = 'Специальное задание';
+
+$MESS['GOING_PEOPLE'] = 'Мобшопперы';
+$MESS['FMS_LEADER_TITLE'] = 'МОБШОППЕРЫ';
+
+$MESS['INVITE_FRIEND'] = 'Пригласи друга';
+$MESS['INVITE'] = 'Invite';
+
+$MESS['PLACE'] = 'Место проведения флэшмоба';
+
+$MESS['MALL_NAME'] = 'Центр';
+$MESS['MALL_ADDRESS'] = 'Адрес';
+$MESS['STORE_NAME'] = 'Оператор';
+$MESS['STORE_LOCATION'] = 'Расположение в центре';
+
+$MESS['MALL_MAP'] = 'Карта молла';
+$MESS['TOWN_MAP'] = 'Карта города';
+$MESS['SHOP_SUBSCRIPTION'] = 'Подписка на оператора';
+
+$MESS['PREVIEW'] = 'Просмотр';
+$MESS['SAVE_DRAFT'] = 'В черновик';
+$MESS['DELETE'] = 'Удалить';
+$MESS['REQUIRED_FIELD'] = 'Обязательные поля';
+$MESS['OPTIONAL_FIELDS'] = 'Необязательные поля';
+$MESS['ERROR_FIELDS'] = 'Поля с ошибками';
+$MESS['PUBLISH'] = 'Разместить';
+$MESS['BACK_TO_EDITING'] = 'Править';
+
+$MESS['CONFIRM_DELETE'] = 'Вы действительно хотите удалить FMS?';
+
+$MESS['SHARE_DISCOUNT'] = 'Скидка';
+$MESS['SHARE_FOR_ACTIVATION_DISCOUNT'] = 'Для активации скидки';
+$MESS['SHARE_WE_COLLECT'] = 'мы собираем';
+$MESS['SHARE_PEOPLE'] = 'чел.';
+$MESS['SHARE_YEAR'] = 'г.';
+$MESS['SHARE_EVENT_WE_COLLECT'] = 'Мы собираем';
+$MESS['SHARE_EVENT_CONDITION'] = 'Условие участия';
+$MESS['SHARE_EVENT_TICKET'] = 'Билет';
+$MESS['SHARE_RETURN_RECEIPT'] = ' Получи скидку и сумей дополнительно вернуть 50% своего чека!';
+
+
+$MESS['SHARE_PEOPLE1'] = 'человек';
+$MESS['SHARE_PEOPLE2'] = 'человека';
+$MESS['SHARE_PEOPLE3'] = 'человек';
+
+$MESS['SHARE_ALREADY_PEOPLE'] = 'Уже идут';
+
+$MESS['DISCOUNT_FOR_FRIENDS'] = 'Скидки для друзей';
+
+$MESS['PLUSO_LANG'] = 'ru';
