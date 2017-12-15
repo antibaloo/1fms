e585fe9d5d073a425e752d4bc92a3307bbc46b39
root <root@1fms.com>
2017-12-15 06:51:53 +0000

    Собственно первая версия компонета альбома мероприятия

--- htdocs/bitrix/components/fms/fms.album/component.php
+++ htdocs/bitrix/components/fms/fms.album/component.php
@@ -1,4 +1,88 @@
 <?
 if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true)die();
+global $USER;
+$arSelect = array(
+	"ID",
+	"NAME",
+	"CODE",
+	"ACTIVE_FROM",
+	"DATE_CREATE",
+	"CREATED_BY",
+	"IBLOCK_ID",
+	"IBLOCK_SECTION_ID",
+	"DETAIL_PAGE_URL",
+	"DETAIL_TEXT",
+	"DETAIL_TEXT_TYPE",
+	"DETAIL_PICTURE",
+	"PREVIEW_TEXT",
+	"PREVIEW_TEXT_TYPE",
+	"PREVIEW_PICTURE",
+	
+);
+
+global $s5BitrixCodes, $fmsesGetter;
+
+$property_enums = CIBlockPropertyEnum::GetList(Array("DEF"=>"DESC", "SORT"=>"ASC"), Array("IBLOCK_ID"=>$s5BitrixCodes->getIblockId('instagram_albums'), "CODE"=>"STATUS"));
+$status_enums = array();
+while($enum_fields = $property_enums->GetNext()) {
+	$status_enums[$enum_fields['XML_ID']] = $enum_fields;
+}
+
+$arResult['TITLE'] = Fms\MallAwardsShareMessages::getMessageByFmsId($arParams['FMS_ID'], 'SHARED_PHOTO');
+
+
+$arResult['URL'] = "/?album={$arResult['FMS']['ID']}&show_lang=".GetMessage('PLUSO_LANG');
+if ($USER->IsAuthorized()){
+	$arResult['URL'] .= '&user='.$USER->GetID();
+}
+$arResult['URL'] = "http://{$_SERVER['HTTP_HOST']}".\Fms\Utils::GetOrAddShortUri($arResult['URL']);
+
+$arFilter = array(
+	"IBLOCK_ID"=>$s5BitrixCodes->getIblockId('instagram_albums'), 
+	'PROPERTY_FMS'=>$arParams['FMS_ID'],
+	'PROPERTY_STATUS' => $status_enums['PUBLISHED']['ID']
+);
+$res = CIBlockElement::GetList(array(), $arFilter, false, false, $arSelect);
+$arResult['PHOTOS'] = array();
+$arResult['VIDEOS'] = array();
+$arResult['COUNT_LIKES'] = 0;
+while($ob = $res->GetNextElement()) {
+	$arFields = $ob->GetFields();
+	$arFields['PROPERTIES'] = $ob->GetProperties();
+	
+	$arFields['URL'] = "/?album={$arParams['FMS_ID']}&photo={$arFields['ID']}&show_lang=".GetMessage('PLUSO_LANG');
+	if ($USER->IsAuthorized()){
+		$arFields['URL'] .= '&user='.$USER->GetID();
+	}
+	$arFields['URL'] = "http://{$_SERVER['HTTP_HOST']}".\Fms\Utils::GetOrAddShortUri($arFields['URL']);
+	
+	if ($arFields['PROPERTIES']['THUMB']['VALUE']){
+		$thumb_id = $arFields['PROPERTIES']['THUMB']['VALUE'];
+	}else{
+		$thumb_id = $arFields['PROPERTIES']['FILE']['VALUE'];
+	}
+	
+	$arFields["PICTURE"] = CFile::GetFileArray($arFields['PROPERTIES']['FILE']['VALUE']);
+
+	$tmp = CFile::ResizeImageGet($thumb_id,array('width'=>150, 'height'=>150), BX_RESIZE_IMAGE_EXACT, true);
+    $arFields["PICTURE"]['THUMB'] =  Fms\ImageUrl::getUploadSrc($tmp['src']);
+    $arFields["PICTURE"]['SRC']  =  Fms\ImageUrl::getUploadSrc($arFields["PICTURE"]['SRC']);
+
+	$arResult['COUNT_LIKES'] += intval($arFields['PROPERTIES']['COUNT_LIKES']['VALUE']);
+	
+	if ($arFields['PROPERTIES']['MEDIA_TYPE']['VALUE_XML_ID'] == 'image'){
+		$arResult['PHOTOS'][] = $arFields;
+	}elseif($arFields['PROPERTIES']['MEDIA_TYPE']['VALUE_XML_ID'] == 'video'){
+		$arFields['USER'] = CUser::GetByID($arFields['PROPERTIES']['USER']['VALUE'])->Fetch();
+		$arResult['VIDEOS'][] = $arFields;
+	}
+} 
+
+
+
+
+
+
+//echo '<pre>'; print_r($arResult); echo '</pre>';
 $this->IncludeComponentTemplate();
 ?>
--- htdocs/bitrix/components/fms/fms.album/templates/.default/template.php
+++ htdocs/bitrix/components/fms/fms.album/templates/.default/template.php
@@ -1,4 +1,39 @@
 <?
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 ?>
-<h2>Фотоальбом</h2>
+<div class="pgPhotos_tabContent pgOverall_overallTabContent pgPhotos_activeTabContent">
+	<div class="photo_container">
+<?foreach (array_merge($arResult['PHOTOS'], $arResult['VIDEOS']) as $item):?>
+		<div class="pgPhoto c-123">
+			<img src="<?=$item['PICTURE']['THUMB']?>" alt="">
+			<?if ($item['PROPERTIES']['MEDIA_TYPE']['VALUE_XML_ID'] == 'video'):?>
+				<a class="pgPhoto_overlay " href="<?=$item['PROPERTIES']['LINK']['VALUE']?>" target="_blank"></a>
+			<?else:?>
+				<a class="pgPhoto_overlay pgOverall_grouped cboxElement" data-album="<?=$item['PROPERTIES']['FMS']['VALUE']?>" data-id="<?=$item['ID']?>" href="<?=$item['PICTURE']['SRC']?>" data-url="<?=$item['URL']?>"data-title="<?=$arResult['TITLE']?>"data-description="<?=$arResult['DESCRIPTION']?>"></a>
+			<?endif?>
+				<div class="pgPhoto_plus <?=Fms\Utils::isLike($item['ID']) ? 'pgPhoto_plus_gray':''?>" data-id="<?=$item['ID']?>"></div>
+		</div>
+		<?endforeach?>
+	</div>
+</div>
+<div class="pgPhotos_Points">
+	<?=GetMessage('POINTS')?>: <?=$arResult['COUNT_LIKES']?>
+</div>
+<script type="text/javascript">
+$(function(){
+	window.pluso.start($('.plusoAlbum'));
+	});
+</script>
+<script>
+$document.ready(function () {
+	$('#pg_photos').ser5Tabs({
+		tabButtonsSelector:    '.pgPhotos_counter',
+		tabContentsSelector:   '.pgPhotos_tabContent',
+		activeTabButtonClass:  'pgPhotos_activeCounter',
+		activeTabContentClass: 'pgPhotos_activeTabContent',
+		tabButtonRelAttr:      'data-tab-content'
+	});
+	$('.pgPhotos_photosTabContent .pgPhoto_grouped').colorbox({rel:'pgPhoto_grouped'});
+	$('.pgOverall_overallTabContent .pgOverall_grouped').colorbox({rel:'pgOverall_grouped'});
+});
+</script>
