68f60f7c0c31b8fb0d093ea37a118c4e4b53f075
root <root@1fms.com>
2018-01-11 13:14:54 +0000

    Показываем лидера при наличие его

--- htdocs/bitrix/components/fms/fms.prize/component.php
+++ htdocs/bitrix/components/fms/fms.prize/component.php
@@ -28,15 +28,46 @@ if ($arResult['prize'] = $res->Fetch()) {//Если есть приз месяц
 		$leaders[] = $arLeader["PROPERTY_USER_VALUE"];
 	}
 	$leaders = array_count_values ($leaders);
-	arsort($leaders);
-	reset($leaders);
-	$arResult['leader']['ID'] = key($leaders);
-	$rsUser = CUser::GetByID(key($leaders));
-	$arUser = $rsUser->Fetch();
-	$arResult['leader']['NAME'] = $arUser['NAME'];
-	$arResult['leader']['LAST_NAME'] = $arUser['LAST_NAME'];
-	$arResult['leader']['PHOTO'] = CFile::GetFileArray($arUser['PERSONAL_PHOTO']);
-	$arResult['leader']['COUNT'] = current($leaders);
+	if ($leaders){
+		arsort($leaders);
+		reset($leaders);
+		$arResult['leader']['ID'] = key($leaders);
+		$rsUser = CUser::GetByID(key($leaders));
+		$arUser = $rsUser->Fetch();
+		$arResult['leader']['NAME'] = $arUser['NAME'];
+		$arResult['leader']['LAST_NAME'] = $arUser['LAST_NAME'];
+		$arResult['leader']['PHOTO'] = CFile::GetFileArray($arUser['PERSONAL_PHOTO']);
+		$arResult['leader']['COUNT'] = current($leaders);
+		$arResult['leader']['show'] = true;
+	}else{
+		$arResult['leader']['show'] = false;
+	}
+	/*Выбор всех мероприятий молла*/
+	$arFilter=array(
+		"IBLOCK_ID"=>$s5BitrixCodes->getIblockId('fmses'),
+		"PROPERTY_MALL" => $arParams ['MALL_ID'],
+	);
+	$arSelect = array("ID");
+	$rsFms = CIBlockElement::GetList(array(), $arFilter, false, false, $arSelect);
+	while ($arFms = $rsFms->Fetch()){$Fmses[] = $arFms['ID'];}
+	$property_enums = CIBlockPropertyEnum::GetList(Array("DEF"=>"DESC", "SORT"=>"ASC"), Array("IBLOCK_ID"=>$s5BitrixCodes->getIblockId('instagram_albums'), "CODE"=>"STATUS"));
+	$status_enums = array();
+	while($enum_fields = $property_enums->GetNext()) {
+		$status_enums[$enum_fields['XML_ID']] = $enum_fields;
+	}
+	/*Выбор опубликованных фото по мероприятиям молла*/
+	$arFilter=array(
+		"IBLOCK_ID"=>$s5BitrixCodes->getIblockId('instagram_albums'),
+		"PROPERTY_FMS" => $Fmses,
+		'PROPERTY_STATUS' => $status_enums['PUBLISHED']['ID']
+	);
+	$arSelect = array("PROPERTY_FMS");
+	$rsAlbums = CIBlockElement::GetList(array(), $arFilter, false, false, $arSelect);
+	while ($arAlbums = $rsAlbums->Fetch()){$fmsAlbums[] = $arAlbums['PROPERTY_FMS_VALUE'];}
+	/*Уникальные ID мероприятий молла с опубликованными альбомами*/
+	$fmsAlbums = array_unique($fmsAlbums);
+	$rand_key = array_rand ($fmsAlbums);
+	$arResult['RANDOM_ALBUM'] = $fmsAlbums[$rand_key];
 	$this->IncludeComponentTemplate();
 }
 ?>
