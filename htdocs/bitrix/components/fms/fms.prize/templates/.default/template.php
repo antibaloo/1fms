4be5d20fa8ee166cf0a92c7ec42f9de5130ffb64
root <root@1fms.com>
2018-01-11 13:12:38 +0000

    Вызов шаблона при наличии лидера + мелкие правки

--- htdocs/bitrix/components/fms/fms.prize/templates/.default/template.php
+++ htdocs/bitrix/components/fms/fms.prize/templates/.default/template.php
@@ -1,15 +1,16 @@
 <?
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 IncludeTemplateLangFile(__FILE__);
+$uri_parts = explode('?', $_SERVER['REQUEST_URI'], 2);
 ?>
-<a href="http://ya.ru" target="_blank" title="<?= GetMessage("MALL_ALBUMS")?>">
+<a href="<?='http://'.$_SERVER['HTTP_HOST'].$uri_parts[0]."?album=".$arResult['RANDOM_ALBUM']?>"title="<?= GetMessage("MALL_ALBUMS")?>">
 	<div class="fmsDetail_prizeOfMonth">
 		<span style="font-weight:bold;font-size:16px;font-family: Arial;"><?= GetMessage("PRIZE_HEADER")?></span>
 		<div class="prizeImageWrapper">
 			<div class="prizeImage">
 				<img src="<?=$arResult['prize']['DETAIL_PICTURE']['SRC']?>" width="auto" height="304px">
 				<div class="logoText"></div>
-				<div class="mallLogo"><img src="<?=$arResult['mallLogo']['SRC']?>"></div>
+				<div class="mallLogo"><img src="<?=$arResult['mallLogo']['SRC']?>" width="auto" height="74px"></div>
 				<div class="prizeText">
 					<div class="prizeSubText"><?=$arResult['prize']['DETAIL_TEXT']?></div>
 				</div>
