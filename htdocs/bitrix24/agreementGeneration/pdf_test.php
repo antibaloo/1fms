28016e0b9f0fc8781cecb9566888a8c8c1f17aa0
root <root@1fms.com>
2018-04-21 12:58:10 +0000

    Мелкие правки

--- htdocs/bitrix24/agreementGeneration/pdf_test.php
+++ htdocs/bitrix24/agreementGeneration/pdf_test.php
@@ -1,9 +1,11 @@
 <?php
 require($_SERVER['DOCUMENT_ROOT'].'/include/fpdf/fpdf.php');
 $pdf=new FPDF();
+$pdf-> AddFont('TimesNRCyrMT','','7454.php');
+$pdf-> SetFont('TimesNRCyrMT','',12);
 //set document properties
-$pdf->SetAuthor('Lana Kovacevic');
-$pdf->SetTitle('FPDF tutorial');
+$pdf->SetAuthor('русский изык');
+$pdf->SetTitle('русский изык');
 //set font for the entire document
 $pdf->SetFont('Helvetica','B',20);
 $pdf->SetTextColor(50,60,100);
@@ -11,7 +13,7 @@ $pdf->SetTextColor(50,60,100);
 $pdf->AddPage('P');
 $pdf->SetDisplayMode(real,'default');
 //insert an image and make it a link
-$pdf->Image('http://chart.apis.google.com/chart?cht=qr&chs=300x300&chl=zdz-online.ru?agreementId=AAAAAB',10,20,33,0,'PNG');
+$pdf->Image('http://chart.apis.google.com/chart?cht=qr&chs=300x300&chl=zdz-online.ru?agreementId='.'AAAAAB',10,20,33,0,'PNG');
 //display the title with a border around it
 $pdf->SetXY(50,20);
 $pdf->SetDrawColor(50,60,100);
@@ -19,7 +21,7 @@ $pdf->Cell(100,10,'FPDF Tutorial',1,0,'C',0);
 //Set x and y position for the main text, reduce font size and write content
 $pdf->SetXY (10,50);
 $pdf->SetFontSize(10);
-$pdf->Write(5,'Congratulations! You have generated a PDF.');
+$pdf->Write(5,'Русские хакеры. Congratulations! You have generated a PDF.');
 //Output the document
-$pdf->Output('example1.pdf','D');
+$pdf->Output('example1.pdf','I');
 ?>
